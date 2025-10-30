3d3bb7e5ae6de4e769309e72f4615fe2e40d25df
Pullie02 <dere.rzgar02@gmail.com>
2025-10-30 15:56:23 +0300

    Added the interactions but dont work yet

--- scripts/NewGame.gd
+++ scripts/NewGame.gd
@@ -35,4 +35,3 @@ func _begin_game(argument: String):
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		get_tree().change_scene_to_file("res://scenes/hub.tscn")
-		print("fe")
