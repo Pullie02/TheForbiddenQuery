dca894043b3689163d5c00047472b8a3535d1d79
Pullie02 <dere.rzgar02@gmail.com>
2025-11-04 20:42:33 +0300

    finally fixed up the shitty SQL level 1

--- scripts/firewall_1.gd
+++ scripts/firewall_1.gd
@@ -1,21 +1,34 @@
 extends Control
 
+var db = SQLite
+var qr
+
 @onready var email: LineEdit = $LineEdit
 @onready var passw: LineEdit = $LineEdit2
 @onready var display_label: Label = $Label
+@onready var button: Button = $Button
 
 func _ready() -> void:
-	# Connect both signals
+	db = SQLite.new()
+	db.path = "res://Database/database.db"
+	db.open_db()
+
+	# Connect signals
 	email.text_changed.connect(_update_label)
 	passw.text_changed.connect(_update_label)
-	
-	# Initialize the label right away
+	button.pressed.connect(_query)
+
 	_update_label()
 
 func _update_label(_new_text := "") -> void:
 	display_label.text = (
-		"SELECT *\n"+
-		"FROM users\n"+
-		"WHERE email = '" + email.text + "'\n"+
+		"SELECT *\n" +
+		"FROM users\n" +
+		"WHERE email = '" + email.text + "'\n" +
 		"AND pass = '" + passw.text + "'"
 	)
+
+func _query(_new_text := "") -> void:
+	var sql = "SELECT * FROM users WHERE email = '" + email.text + "' AND pass = '" + passw.text + "'"
+	db.query(sql)
+	print(db.query_result)
