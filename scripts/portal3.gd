47f18164be93072953d937ac98cfa8d3aa82f988
Pullie02 <dere.rzgar02@gmail.com>
2025-11-23 00:48:44 +0300

    lvl2 fr done

---
+++ scripts/portal3.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var interactable: Area2D = $Interactable
+
+func _ready():
+	interactable.interact = _on_interact
+	
+func _on_interact():
+	get_tree().change_scene_to_file("res://scenes/lvls/lvl3/level_3.tscn")
